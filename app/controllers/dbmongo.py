c46b48542405119d4b16957bb373a59cdc0c4fa1
Bruno Leal <bruno.carmo93@gmail.com>
2019-05-24 13:56:06 -0300

    atualizacao

--- app/controllers/dbmongo.py
+++ app/controllers/dbmongo.py
@@ -1,4 +1,4 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, ASCENDING
 #from cotacaoTeste import searchDerivativos
 
 client = MongoClient('mongodb://broadcast:agestado@cluster0-shard-00-00-umdst.mongodb.net:27017,cluster0-shard-00-01-umdst.mongodb.net:27017,cluster0-shard-00-02-umdst.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true')
@@ -211,7 +211,7 @@ class searchCotacao:
 
         def searchDerivativos(self, data):
                 if 'TUDO' in data:
-                        result = self.coll.find()
+                        result = self.coll.find({}).sort([('mercadoria', ASCENDING), ('fonte', ASCENDING), ('mercado', ASCENDING)])
 
                         return result
                         
